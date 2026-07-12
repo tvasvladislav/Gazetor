e61930c819a730ef89b31b804a5ef11a9a8db3d3
tvasvladislav <tvas.vladislav@gmail.com>
2026-07-12 20:45:26 +0300

    Update Telegram message text and log responses

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
+print("CHAT_ID =", CHAT_ID)
+
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
+response = requests.post(
     url,
     json={
         "chat_id": CHAT_ID,
-        "text": "✅ Gazetor успешно запущен через GitHub Actions!"
+        "text": "✅ Gazetor работает!"
     }
 )
+
+print("Status:", response.status_code)
+print("Response:", response.text)
