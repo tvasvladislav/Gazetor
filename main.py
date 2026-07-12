913e147e6774f43f23d2647583554564c4f29d5c
tvasvladislav <tvas.vladislav@gmail.com>
2026-07-12 20:32:12 +0300

    Add Telegram notification on bot startup
    Send a message to Telegram when the bot is started.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+import requests
 
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+requests.post(
+    url,
+    json={
+        "chat_id": CHAT_ID,
+        "text": "✅ Gazetor успешно запущен через GitHub Actions!"
+    }
+)
